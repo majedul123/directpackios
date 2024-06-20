b7086c1884bb5c1b102d03247d056265922e3fef
majedul123 <majedul.islam@brainstation-23.com>
2024-06-20 12:23:52 +0600

    Initial Commit

---
+++ Direct Packaging/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Direct Packaging
+//
+//  Created by Majedul on 6/20/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
